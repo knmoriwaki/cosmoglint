4e2717c5c75fb775de47caebb4380302c8954f03
knmoriwaki <kn.moriwaki@gmail.com>
2025-09-29 17:27:47 +0900

    Modified training functions for readability

--- scripts/train_transformer.py
+++ scripts/train_transformer.py
@@ -25,11 +25,6 @@ def parse_args():
     parser.add_argument("--output_dir", type=str, default="output")
     parser.add_argument("--seed", type=int, default=12345)
 
-    parser.add_argument("--max_length", type=int, default=30)
-
-    parser.add_argument("--input_features", type=str, nargs='+', default=["GroupMass"])
-    parser.add_argument("--output_features", type=str, nargs='+', default=["SubhaloSFR", "SubhaloDist", "SubhaloVrad", "SubhaloVtan"])
-    parser.add_argument("--global_features", type=str, nargs='+', default=None)
 
     # dataset parameters
     parser.add_argument("--data_path", type=str, nargs='+', default=["data.h5"], help="Path to the data file(s). If the first file contains '*', indices will be used to specify the files, and other files will be ignored.")
@@ -37,6 +32,12 @@ def parse_args():
     parser.add_argument("--norm_param_file", type=str, default="./norm_params.json")
     parser.add_argument("--global_param_file", type=str, nargs='+', default=None, help="Path to the global parameters file(s). If not None, the number of files must match that of data_path.")
 
+    parser.add_argument("--input_features", type=str, nargs='+', default=["GroupMass"])
+    parser.add_argument("--output_features", type=str, nargs='+', default=["SubhaloSFR", "SubhaloDist", "SubhaloVrad", "SubhaloVtan"])
+    parser.add_argument("--global_features", type=str, nargs='+', default=None)
+    parser.add_argument("--max_length", type=int, default=30)
+
+    # training parameters
     parser.add_argument("--train_ratio", type=float, default=0.9)
     parser.add_argument("--batch_size", type=int, default=128)
     parser.add_argument("--num_epochs", type=int, default=2)
@@ -99,7 +100,6 @@ def train_model(args):
         if global_params is not None:
             global_params = global_params[istart:iend+1, :]
 
-        
     dataset = MyDataset(args.data_path, args.input_features, args.output_features, global_params=global_params, norm_param_dict=norm_param_dict, max_length=args.max_length, exclude_ratio=args.exclude_ratio)
     train_size = int(args.train_ratio * len(dataset))
     val_size = len(dataset) - train_size
@@ -140,11 +140,23 @@ def train_model(args):
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=1e-5)
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.num_epochs, eta_min=1e-6)
 
-    def loss_func(output, target, mask, weight=None):
-        # output: (batch, seq_length, num_features_in, num_features_out)
-        # target: (batch, seq_length, num_features_in)
-        # mask: (batch, seq_length, num_features_in)
-        # weight: (batch, seq_length, num_features_in)
+    def loss_func(batch, weight=None):
+
+        context, seq, global_cond, mask = batch
+
+        context = context.to(device)  # (batch, num_condition)   
+        seq = seq.to(device)     # (batch, max_length, num_features_in)
+        global_cond = None if global_cond is None else global_cond.to(device) # (batch, num_features_global)
+        mask = mask.to(device)   # (batch, max_length)
+        #weight = (6.7 * context[:,0]).pow(10).detach()
+        #weight = weight / weight.mean()
+
+        input_seq = seq[:, :-1]
+        target = seq
+
+        output = model(context, input_seq, global_cond=global_cond) # (batch, max_length, num_features_in, num_features_out)
+        #_, output = model.generate(context, seq=seq, teacher_forcing_ratio=teacher_forcing_ratio) 
+        # output: (batch, max_length, num_features_in, num_features_out)
 
         batch_size, seq_length, num_features_in, num_features_out = output.shape
         if weight is None:
@@ -152,7 +164,7 @@ def train_model(args):
 
         weight = mask * weight
 
-        log_prob = torch.log( output + 1e-8 )    
+        log_prob = torch.log( output + 1e-8 )
         target_bins = (target * num_features_out).long() # (batch, seq_length, num_features_in) [0, 1] -> [0, num_features_out-1]
         target_bins = torch.clamp(target_bins, min=0, max=num_features_out - 1)
 
@@ -163,7 +175,13 @@ def train_model(args):
         loss_nll = F.nll_loss(log_prob_flatten, target_bins_flatten, reduction='none') 
         loss = (loss_nll * weight_flatten).sum() / ( (weight_flatten).sum() + 1e-8 )
 
-        return loss
+        if args.lambda_penalty_loss > 0:
+            loss_penalty = args.lambda_penalty_loss * loss_func_penalty(output, seq, mask)
+            loss += loss_penalty
+        else:
+            loss_penalty = None
+
+        return loss, loss_penalty
 
     def loss_func_penalty(output, target_bins, mask, weight=None):
 
@@ -191,54 +209,24 @@ def train_model(args):
         for epoch in tqdm(range(args.num_epochs), file=sys.stderr):
             model.train()
 
-            for count, (context, seq, global_cond, mask) in enumerate(train_dataloader):
-                context = context.to(device)  # (batch, num_condition)   
-                seq = seq.to(device)     # (batch, max_length, num_features_in)
-                global_cond = None if global_cond is None else global_cond.to(device) # (batch, num_features_global)
-                mask = mask.to(device)   # (batch, max_length)
-                #weight = (6.7 * context[:,0]).pow(10).detach()
-                #weight = weight / weight.mean()
-                
+            for count, batch in enumerate(train_dataloader):
                 optimizer.zero_grad()
-
-                input_seq = seq[:, :-1]
-                output = model(context, input_seq, global_cond=global_cond) # (batch, max_length, num_features_in, num_features_out)
-                #_, output = model.generate(context, seq=seq, teacher_forcing_ratio=teacher_forcing_ratio) 
-                # output: (batch, max_length, num_features_in, num_features_out)
-                
-                loss = loss_func(output, seq, mask) #, weight=weight)
-
-                if args.lambda_penalty_loss > 0:
-                    loss_penalty = args.lambda_penalty_loss * loss_func_penalty(output, seq, mask)
-                    loss += loss_penalty
+                loss, loss_penalty = loss_func(batch) #, weight=weight)
 
                 loss.backward()
                 optimizer.step()
 
                 model.eval()
-                for context_val, seq_val, global_cond_val, mask_val in val_dataloader:
+                for batch_val in val_dataloader:
                     with torch.no_grad():
-                        context_val = context_val.to(device)
-                        seq_val = seq_val.to(device)
-                        global_cond_val = None if global_cond_val is None else global_cond_val.to(device)
-                        mask_val = mask_val.to(device)
-                        #weight = (6.7 * context_val[:,0]).pow(10).detach()
-                        
-                        input_seq_val = seq_val[:, :-1]
-                        output_val = model(context_val, input_seq_val, global_cond=global_cond_val)
-                        loss_val = loss_func(output_val, seq_val, mask_val)
-
-                        if args.lambda_penalty_loss > 0:
-                            loss_penalty_val = args.lambda_penalty_loss * loss_func_penalty(output_val, seq_val, mask_val)
-                            loss_val += loss_penalty_val
-                    
+                        loss_val, loss_penalty_val = loss_func(batch_val)
                         break # show one batch result only
                 model.train()
 
                 epoch_now = epoch + count / num_batches
                 
                 log = "{:.8f} {:.4f} {:.4f} ".format(epoch_now, loss.item(), loss_val.item())
-                if args.lambda_penalty_loss > 0:
+                if loss_penalty_val is not None:
                     log += "{:.4f} {:.4f} ".format(epoch_now, loss_penalty.item(), loss_penalty_val.item())
                 f.write("{}\n".format(log))
 
