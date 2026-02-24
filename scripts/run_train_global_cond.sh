0e0348e22bf5292e3c07f5c8c1c076c30e0b2005
knitoskr <kanoito2423@g.ecc.u-tokyo.ac.jp>
2026-02-24 12:12:24 +0900

    fixed a minor bug

--- scripts/run_train_global_cond.sh
+++ scripts/run_train_global_cond.sh
@@ -11,12 +11,13 @@ do
     do
         for num_epochs in 40 
         do
-            output_dir=./runs/output_transformer/$transformer_${snapshot_number}_ep${num_epochs}_bs${batch_size}_w${sampler_weight_min}
+            model_name=transformer1_with_global_cond
+            output_dir=./runs/output_transformer/${model_name}_${snapshot_number}_ep${num_epochs}_bs${batch_size}_w${sampler_weight_min}
             
             mkdir -p $output_dir/source
             cp -r train_transformer.py $config_file ../cosmoglint/model/transformer.py ../cosmoglint/utils $output_dir/source/.
 
-            python3 train_transformer.py --gpu_id $gpu_id --config_file $config_file --output_dir $output_dir --batch_size $batch_size --num_epochs $num_epochs --dropout 0 --sampler_weight_min $sampler_weight_min --save_freq 10
+            python3 train_transformer.py --gpu_id $gpu_id --config_file $config_file --output_dir $output_dir --batch_size $batch_size --num_epochs $num_epochs --dropout 0 --sampler_weight_min $sampler_weight_min --save_freq 10 --no-show_pbar
 
         done
     done
